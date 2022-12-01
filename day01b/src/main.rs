e02c4f8c5aa4d48ea47e74201efa2bd1cf7cb45d
Daniel Kenyon-Jones <github@dlkj.co.uk>
2022-12-01 15:26:36 +0000

    day01b and update template

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -16,8 +16,13 @@ fn solve(input: &str) -> Result<u32, ParseIntError> {
         .group_by(|&l| !l.is_empty())
         .into_iter()
         .filter_map(|(b, g)| b.then(|| g.map(str::parse::<u32>).sum()))
-        .collect::<Result<Vec<_>, _>>()
-        .map(|v| v.into_iter().max().unwrap_or(0))
+        .collect::<Result<Vec<u32>, _>>()
+        .map(|v| {
+            v.into_iter()
+                .sorted_unstable_by(|a, b| b.cmp(a))
+                .take(3)
+                .sum()
+        })
 }
 
 #[cfg(test)]
@@ -26,6 +31,6 @@ mod tests {
     #[test]
     fn example() {
         let result = solve(include_str!("../example.txt"));
-        assert_eq!(result, Ok(24000));
+        assert_eq!(result, Ok(45000));
     }
 }
--- daytemplate/src/main.rs
+++ daytemplate/src/main.rs
@@ -1,9 +1,24 @@
-use anyhow::Ok;
+use std::num::ParseIntError;
 
-fn main() -> Result<(), anyhow::Error> {
+fn main() -> Result<(), ParseIntError> {
     let input = include_str!("../input.txt");
 
-    println!("{input}");
+    println!("{}", solve(input)?);
 
     Ok(())
+}
+
+fn solve(input: &str) -> Result<u32, ParseIntError> {
+    input.parse()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn example() {
+        let result = solve(include_str!("../example.txt"));
+        assert_eq!(result.is_err(), true);
+    }
 }
